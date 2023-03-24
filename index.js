f0156aba2e299a89e206ef9453fff592a9906830
Alfonso de la Osa <fonz@seniorly.com>
2023-03-24 20:00:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -23,28 +23,12 @@ const run = async () => {
   });
 
   const prReviews = reviews.data;
-  if (prReviews.length === 0) {
-    const statusPR = await client.request('GET /repos/{owner}/{repo}/pulls/{pull_number}', {
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-      pull_number: github.context.payload.pull_request.number,
-    });
-
-    const status = statusPR.data.state;
-    const mergedStatus = statusPR.data.merged;
-    core.debug(`Merge status is: ${mergedStatus}`);
-    if (mergedStatus)
-      prState = 'MERGED';
-    else
-      prState = status.toUpperCase();
+  if (pullRequest.merged) {
+    prState = 'MERGED';
+  } else if (prReviews.length > 0) {
+    prState = prReviews[prReviews.length - 1].state.toUpperCase();
   } else {
-    const state = prReviews[prReviews.length - 1].state;
-    const mergedStatus = prReviews[prReviews.length - 1].merged;
-    core.debug(`Merge status is: ${mergedStatus}`);
-    if (mergedStatus)
-      prState = 'MERGED';
-    else
-      prState = state.toUpperCase();
+    prState = state.toUpperCase();
   }
 
   core.info(prState);
